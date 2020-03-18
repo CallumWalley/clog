952798b45322fdc6b6b2464bdef53a76e55a1ac3
Callum Walley <callum.walley@nesi.org.nz>
2020-03-19 08:44:27 +1300

    Made package

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .log import logbject as log
+# Settings relative to MODULE no calling script
--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+git pull origin master
+
+module load Python/3.8.2-gimkl-2020a
 pip3 install --user slackclient
 
+if [ ! -f "config.json" ]; then
+    echo `{"api_token":"","maintainers":{},"channel":"","path":"/log.txt"}` > "config.json"
+fi
